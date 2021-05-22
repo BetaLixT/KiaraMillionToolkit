c990a7af6760c0b45c97175bed0908bcffe529a7
BetaLixT <dcruzalphin@gmail.com>
2021-05-22 10:28:30 +0400

    initial

---
+++ KiaraImageSizeCalc/KiaraImageSizeCalc/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Drawing;
+
+namespace KiaraImageSizeCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var canvasImagesJson = System.IO.File.ReadAllText("canvasImages.json");
+            var canvasImages = JsonConvert.DeserializeObject<List<CanvasImage>>(canvasImagesJson);
+            for(int i = 0; i < canvasImages.Count; i++)
+            {
+                var image = Image.FromFile(canvasImages[i].src.Substring(1));
+                canvasImages[i].height = (double)image.Height / (double)image.Width;
+            }
+            var canvasImagesUpdateJson = JsonConvert.SerializeObject(canvasImages);
+            System.IO.File.WriteAllText("canvasImages-updated.json", canvasImagesUpdateJson);
+        }
+    }
+
+    class CanvasImage
+    {
+        public string src { get; set; }
+        public double width { get; set; }
+        public double height { get; set; }
+        public double left { get; set; }
+        public double top { get; set; }
+        public double angle { get; set; }
+        public double opacity { get; set; }
+        public double scaleX { get; set; }
+        public double scaleY { get; set; }
+        public string message { get; set; }
+        public string artist { get; set; }
+    }
+}
---
+++ KiaraMilMessageDuplicateCheck/KiaraMilMessageDuplicateCheck/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace KiaraMilMessageDuplicateCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var messages = JsonConvert.DeserializeObject<List<Message>>(System.IO.File.ReadAllText("messages.json"));
+            var secondList = new List<Message>();
+            var duplicateMessage = new List<Message>();
+            var duplicateAuthor = new List<Message>();
+
+            foreach(var message in messages)
+            {
+                if (secondList.Where(x => x.message == message.message).Count() != 0)
+                {
+                    duplicateMessage.Add(message);
+                }
+                else if (secondList.Where(x => x.author == message.author).Count() != 0)
+                {
+                    duplicateAuthor.Add(message);
+                }
+                else
+                {
+                    secondList.Add(message);
+                }
+            }
+        }
+    }
+
+    struct Message
+    {
+        public float width { get; set; }
+        public float height { get; set; }
+        public string message { get; set; }
+        public string author { get; set; }
+    }
+}
